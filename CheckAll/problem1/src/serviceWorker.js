3999fe7885dabac8a56d3f290b38055f3c040b1f
paristhecity <pllopez@wpi.edu>
2019-05-19 20:22:17 -0400

    Refactoring

--- CheckAll/problem1/src/serviceWorker.js
+++ CheckAll/problem1/src/serviceWorker.js
@@ -99,7 +99,7 @@ function registerValidSW(swUrl, config) {
 }
 
 function checkValidServiceWorker(swUrl, config) {
-  // Check if the service worker can be found. If it can't reload the page.
+  // CheckAll if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
     .then(response => {
       // Ensure service worker exists, and that we really are getting a JS file.
--- README.md
+++
@@ -1,2 +0,0 @@
-# MathSpringNewProblems
-Adding to the already existing MathSpring, the code included in this repository will increase the problem types available on the website. 
